45fd01601e089e687ee30f4785a776f1b351f0e3
Ic3Sandy <monkey_king2@hotmail.com>
2022-12-20 14:58:26 +0700

    Add api for update by id

--- server.go
+++ server.go
@@ -123,5 +123,22 @@ func main() {
 		return c.JSON(http.StatusOK, expense)
 	})
 
+	e.PUT("/expenses/:id", func(c echo.Context) error {
+		id := c.Param("id")
+		expense := new(Expense)
+		if err = c.Bind(expense); err != nil {
+			return c.String(http.StatusBadRequest, "bad request")
+		}
+		_, err := db.Exec(`
+			UPDATE expenses
+			SET title = $1, amount = $2, note = $3, tags = $4
+			WHERE id = $5;
+		`, expense.Title, expense.Amount, expense.Note, pq.Array(expense.Tags), id)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, err.Error())
+		}
+		return c.JSON(http.StatusOK, expense)
+	})
+
 	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
 }

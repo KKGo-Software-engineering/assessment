6386e2826d9b1b0f0048df1d564b72b1d80b8194
Ic3Sandy <monkey_king2@hotmail.com>
2022-12-20 14:40:06 +0700

    Add api for create and read

--- server.go
+++ server.go
@@ -7,8 +7,17 @@ import (
 	"os"
 
 	"github.com/labstack/echo"
+	"github.com/lib/pq"
 	_ "github.com/lib/pq"
 )
+
+type Expense struct {
+	ID     int      `json:"id"`
+	Title  string   `json:"title"`
+	Amount float64  `json:"amount"`
+	Note   string   `json:"note"`
+	Tags   []string `json:"tags"`
+}
 
 func main() {
 	fmt.Println("Please use server.go for main file")
@@ -43,5 +52,60 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
+	e.GET("/expenses", func(c echo.Context) error {
+		rows, err := db.Query(`
+			SELECT id, title, amount, note, tags
+			FROM expenses
+			ORDER BY id;
+		`)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, err.Error())
+		}
+		defer rows.Close()
+
+		var expenses []Expense
+		for rows.Next() {
+			var id int
+			var title string
+			var amount float64
+			var note string
+			var tags []string
+			if err := rows.Scan(&id, &title, &amount, &note, (*pq.StringArray)(&tags)); err != nil {
+				return c.JSON(http.StatusInternalServerError, err.Error())
+			}
+			expenses = append(expenses, Expense{
+				ID:     id,
+				Title:  title,
+				Amount: amount,
+				Note:   note,
+				Tags:   tags,
+			})
+		}
+		if err := rows.Err(); err != nil {
+			return c.JSON(http.StatusInternalServerError, err.Error())
+		}
+
+		if expenses == nil {
+			expenses = []Expense{}
+		}
+		return c.JSON(http.StatusOK, expenses)
+	})
+
+	e.POST("/expenses", func(c echo.Context) error {
+		expense := new(Expense)
+		if err = c.Bind(expense); err != nil {
+			return c.String(http.StatusBadRequest, "bad request")
+		}
+		_, err := db.Exec(`
+			INSERT INTO expenses (title, amount, note, tags)
+			VALUES ($1, $2, $3, $4);
+		`, expense.Title, expense.Amount, expense.Note, pq.Array(expense.Tags))
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, err.Error())
+		}
+		return c.JSON(http.StatusOK, expense)
+	})
+
 	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
 }

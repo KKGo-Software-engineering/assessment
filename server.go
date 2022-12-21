f340f0fd9de3d22b9c74988469ed497313b6095e
Ic3Sandy <monkey_king2@hotmail.com>
2022-12-21 17:57:07 +0700

    Add graceful shutdown

--- server.go
+++ server.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
+	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/Ic3Sandy/assessment/expenses"
 	"github.com/labstack/echo"
@@ -25,5 +29,19 @@ func main() {
 	e.GET("/expenses/:id", expenses.GetExpensesById)
 	e.POST("/expenses", expenses.CreateExpense)
 	e.PUT("/expenses/:id", expenses.UpdateExpense)
-	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
+
+	go func() {
+		if err := e.Start(":" + os.Getenv("PORT")); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }

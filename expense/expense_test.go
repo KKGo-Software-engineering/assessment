89867fdba5b7cadfbc0f5c7e01456bfb485740cd
atompsv <phonesavanh.litd@gmail.com>
2022-12-22 08:37:11 +0700

    Add successful testing of creating expense

---
+++ expense/expense_test.go
@@ -0,0 +1,35 @@
+package expense_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/atompsv/assessment/expense"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateExpense(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		mockRepo := expense.NewMockRepository()
+		service := expense.NewService(mockRepo)
+		want := expense.Expense{
+			ID:     "1",
+			Title:  "title",
+			Amount: 1,
+			Note:   "note",
+			Tags:   []string{"tag1", "tag2"},
+		}
+		ex := expense.Expense{
+			Title:  "title",
+			Amount: 1,
+			Note:   "note",
+			Tags:   []string{"tag1", "tag2"},
+		}
+		mockRepo.On("SaveExpense", ex).Return(want, nil).Once()
+
+		got, err := service.CreateExpense(context.Background(), ex)
+
+		assert.NoError(t, err)
+		assert.Equal(t, want, got)
+	})
+}

f75f3404937b167f79ff4f5294c9d0a32d7a0814
atompsv <phonesavanh.litd@gmail.com>
2022-12-22 08:28:58 +0700

    Add expense domain model

---
+++ expense/expense.go
@@ -0,0 +1,15 @@
+package expense
+
+// Expense represents the cost incurred by a user for a particular purpose.
+type Expense struct {
+	// ID is the unique identifier for the expense.
+	ID string `json:"id"`
+	// Title is the name of the expense.
+	Title string `json:"title"`
+	// Amount is the cost of the expense.
+	Amount float64 `json:"amount"`
+	// Note is a description of the expense.
+	Note string `json:"note"`
+	// Tags are the list of tags associated with the expense.
+	Tags []string `json:"tags"`
+}

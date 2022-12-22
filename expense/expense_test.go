06a5b83c79f439b5df581be7750ba9b4078d4653
atompsv <phonesavanh.litd@gmail.com>
2022-12-22 08:38:14 +0700

    Add invalid arguments testing of creating expense

--- expense/expense_test.go
+++ expense/expense_test.go
@@ -32,4 +32,20 @@ func TestCreateExpense(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Equal(t, want, got)
 	})
+
+	t.Run("invalid args", func(t *testing.T) {
+		mockRepo := expense.NewMockRepository()
+		service := expense.NewService(mockRepo)
+		ex := expense.Expense{
+			Title:  "",
+			Amount: 0,
+			Note:   "note",
+			Tags:   []string{"tag1", "tag2"},
+		}
+
+		got, err := service.CreateExpense(context.Background(), ex)
+
+		assert.Equal(t, expense.ErrInvalidArgs, err)
+		assert.Equal(t, expense.Expense{}, got)
+	})
 }

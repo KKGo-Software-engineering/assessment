18b12ee89493d736e6fd2a5f643b7ecc0960ac63
ittikorn chawkamud <aseaways1234@gmail.com>
2022-12-28 04:31:49 +0700

    increate integration test updateExpense

--- pkg/expense/update_it_test.go
+++ pkg/expense/update_it_test.go
@@ -3,6 +3,34 @@
 
 package expense
 
-func TestUpdateExpenseByIdSuccess() {
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUpdateExpenseByIdSuccess(t *testing.T) {
+	body := bytes.NewBufferString(`{
+		"title":"pay market",
+		"amount": 9999.00,
+		"note": "clear debt",
+		"tags": ["markets", "debt"]
+	}`)
+
+	var update Expense
+
+	res := request(http.MethodPut, uri(fmt.Sprintf("expenses/%d", 1)), body)
+	err := res.Decode(&update) // ใช้ decode ข้อมูลที่ได้จาก response body มาเก็บไว้ในตัวแปร u
+
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusOK, res.StatusCode)
+	assert.Equal(t, update.ID, 1)
+	assert.Equal(t, update.Title, "pay market")
+	assert.Equal(t, update.Amount, 9999.00)
+	assert.Equal(t, update.Note, "clear debt")
+	assert.Equal(t, update.Tags, []string{"markets", "debt"})
 
 }

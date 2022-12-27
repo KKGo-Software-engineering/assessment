c04fb24f03c707fa4516d7f7d32c79d5199e55fc
ittikorn chawkamud <aseaways1234@gmail.com>
2022-12-28 04:28:01 +0700

    update unit test updateExpense

---
+++ pkg/expense/update_it_test.go
@@ -0,0 +1,8 @@
+//go:build integration
+// +build integration
+
+package expense
+
+func TestUpdateExpenseByIdSuccess() {
+
+}
--- pkg/expense/update_test.go
+++ pkg/expense/update_test.go
@@ -50,3 +50,101 @@ func TestUpdateExpenseByIDSuccess(t *testing.T) {
 		assert.Equal(t, expected, strings.TrimSpace(rec.Body.String()))
 	}
 }
+
+func TestUpdateExpenseByIDNotFoundParamID(t *testing.T) {
+	// Arrange
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPut, "/", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	mock.ExpectQuery("UPDATE expenses").
+		WillReturnError(sqlmock.ErrCancelled)
+
+	h := handler{db}
+	c := e.NewContext(req, rec)
+	c.SetPath("/expense/:id")
+	c.SetParamNames("id")
+	c.SetParamValues("")
+
+	// Act
+	err = h.UpdateExpense(c)
+
+	// Assertions
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusBadRequest, rec.Code)
+		assert.Equal(t, `{"message":"Invalid request param id"}`, strings.TrimSpace(rec.Body.String()))
+	}
+}
+
+func TestUpdateExpenseByIDIncorrectBody(t *testing.T) {
+	// Arrange
+	e := echo.New()
+	body := `{"TitleAmongUs: "I am impostor"}`
+	req := httptest.NewRequest(http.MethodPut, "/", strings.NewReader(body))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	mock.ExpectQuery("UPDATE expenses").
+		WillReturnError(sqlmock.ErrCancelled)
+
+	h := handler{db}
+	c := e.NewContext(req, rec)
+	c.SetPath("/expense/:id")
+	c.SetParamNames("id")
+	c.SetParamValues("7")
+
+	// Act
+	err = h.UpdateExpense(c)
+
+	// Assertions
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusBadRequest, rec.Code)
+		assert.Equal(t, `{"message":"Invalid request body"}`, strings.TrimSpace(rec.Body.String()))
+	}
+}
+
+func TestUpdateExpenseByIDFails(t *testing.T) {
+	// Arrange
+	e := echo.New()
+	body := `{"Title":"check update","Amount":1000.00,"Note":"note","Tags":["salary"]}`
+	req := httptest.NewRequest(http.MethodPut, "/", strings.NewReader(body))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	mock.ExpectQuery("UPDATE expenses").
+		WillReturnError(sqlmock.ErrCancelled)
+
+	h := handler{db}
+	c := e.NewContext(req, rec)
+	c.SetPath("/expense/:id")
+	c.SetParamNames("id")
+	c.SetParamValues("7")
+
+	// Act
+	err = h.UpdateExpense(c)
+
+	// Assertions
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusBadRequest, rec.Code)
+		assert.Equal(t, `{"message":"canceling query due to user request"}`, strings.TrimSpace(rec.Body.String()))
+	}
+}

a8d318dee0e7fcd63d19febbad4034b1c4c88f0a
Sutthiphong Suwanich <sutthiphong.suw@ascendcorp.com>
2023-01-06 06:13:56 +0700

    update go test to check from github

--- rest/handler/handler_it_test.go
+++ rest/handler/handler_it_test.go
@@ -18,6 +18,7 @@ import (
 	"github.com/labstack/echo/v4"
 	_ "github.com/lib/pq"
 	"github.com/stretchr/testify/assert"
+
 )
 
 const serverPort = 2565
@@ -323,7 +324,7 @@ func TestITCreateExpenseID2(t *testing.T) {
 	}
 
 	// Arrange
-	reqBody := `{"title": "iPhone 14 Pro Max 1TB","amount": 66900,"note": "birthday gift from my love","tags": ["gadget"]}`
+	reqBody := `{"title": "iPhone 14 Pro Max 1TB x","amount": 66900,"note": "birthday gift from my love","tags": ["gadget"]}`
 	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("http://localhost:%d/expenses", serverPort), strings.NewReader(reqBody))
 	assert.NoError(t, err)
 	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
